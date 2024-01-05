6a0f00290b1788f0b81280dcc9731e7552ae0b97
Pinaki19 <pinakibanerjee056@gmail.com>
2024-01-06 03:03:04 +0530

    fill route issue

--- routes/routeHandler.js
+++ routes/routeHandler.js
@@ -4,16 +4,16 @@ const {
     incrementFilledCount,
     incrementTimeSaved
 } = require('./increamentCounts')
-function processAndFillRouteHandler(req, res) {
+async function processAndFillRouteHandler(req, res) {
     try {
         const { Request_NO, Form, Form_data, Total } = req.body;
         main(Request_NO, Form, Form_data, Total);
         res.json({ status: 'ok' });
         const n = parseInt(Total, 10);
-        incrementAnsweredCount(n);
-        incrementFilledCount(n);
+        await incrementAnsweredCount(n);
+        await incrementFilledCount(n);
         const time = Math.ceil((n * 15) / 60);
-        incrementTimeSaved(time);
+        await incrementTimeSaved(time);
     } catch (error) {
         console.error('Error in /fill route handler:', error);
         res.status(500).json({ status: 'error', message: 'Internal Server Error' });
