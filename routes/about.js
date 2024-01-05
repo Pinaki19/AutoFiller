6729b7a485ddf01401b907fe6f9d39a3ed50a4b2
Pinaki19 <pinakibanerjee056@gmail.com>
2024-01-05 16:10:59 +0530

    about section

--- routes/about.js
+++ routes/about.js
@@ -2,11 +2,9 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 /* GET home page. */
-router.get('/contact', function (req, res, next) {
+router.get('/', function (req, res, next) {
     const htmlFilePath = path.join(__dirname, '../public/html', 'about.html');
     res.sendFile(htmlFilePath);
 });
-
-
 
 module.exports = router;
